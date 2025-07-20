a80ce5024d6c70ec0ad2e0fc97a08cc54a0467e0
Matheus Sousa <ma.sousa.soares@gmail.com>
2025-07-20 17:25:47 -0300

    Add project files.

---
+++ Configuration/TokenConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace AspNetJWT.Configuration
+{
+    public class TokenConfiguration
+    {
+        public static string PrivateKey { get; set; } = "?FlsywM$&f|}L@=-H/Z25j2&R~cP91&j~;o:|£";
+    }
+}
